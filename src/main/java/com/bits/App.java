0e0064e73c3d56b27a0dd04f0a215d4d97cc1055
2022wb86080-ui <2022wb86080@wilp.bits-pilani.ac.in>
2026-09-22 20:14:46 +0530

    Update App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world! Making changes");
+        System.out.println("Hello world! Making changes Making Changes");
     }
 }
